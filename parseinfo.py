05ed40ed4750a296a7c19ec3dc66f8c8aa6d4faf
Joe Polastre <joe.polastre@airbus-sv.com>
2020-05-08 16:42:22 -0700

    Significant improvements in parsing support, normalization of results

--- parseinfo.py
+++ parseinfo.py
@@ -1,6 +1,7 @@
 import argparse
 from enum import Enum
 from dateutil.parser import parse as dateparse
+import utils
 import os
 import re
 
@@ -8,8 +9,9 @@ DEBUG = False
 WRITE_TAGS = False
 
 EXT = ".flac"
-TITLE_RE = re.compile(r'^(d\dt){0,1}(?P<num>\d{1,2})[:\.\,\s]*(?P<name>([\w\s\"\-\(\)\/\[\]<>.,?\'’^>#&]|E:)+)(\s*[~!@#$%^&*+=?>]+)*(((\s{2,}[-]{1,2}\s*)|\s+)[[{]{0,1}(?P<time>\d{1,2}[:\.]\d{2}([:\.]\d{2,3})?){0,1}[])]{0,1}){0,1}$')
-LOCATION_RE = re.compile(r'([\w\s\.]+)(,){0,1} (\w\.{0,1}\w\.{0,1})$')
+TITLE_RE = re.compile(r'^(d\dt){0,1}(?P<num>\d{1,2})[:\.\,\s]*(?P<name>(([\w\s\"\-\(\)\/\[\]<>.,?\'’^+#&])|(E\d*:))+)(\s*(([~!@#$%^&*+=?>])|(-->))+)*(((\s{2,}[-]{1,2}\s*)|\s+)[[{]{0,1}(?P<time>\d{1,2}[:\.]\d{2}([:\.]\d{2,3})?){0,1}[])]{0,1}){0,1}$')
+# TITLE_RE = re.compile(r'^(d\dt){0,1}(?P<num>\d{1,2})[:\.\,\s]*(?P<name>([\w\s\"\-\(\)\/\[\]<>.,?\'’^>#&]|E:)+)(\s*(([~!@#$%^&*+=?>])|(-->))+)*(((\s{2,}[-]{1,2}\s*)|\s+)[[{]{0,1}(?P<time>\d{1,2}[:\.]\d{2}([:\.]\d{2,3})?){0,1}[])]{0,1}){0,1}$')
+LOCATION_RE = re.compile(r'(?P<city>[\w\s\.]+)((,\s*)|(\s+))(?P<state>\w\.{0,1}\w\.{0,1})$')
 DISC_RE = re.compile(r'^(\d{0,2}\s+((dis[ck])|(cd)))|(((dis[ck])|(cd))\s+\d{0,2})', re.I)
 
 RED = "\033[31m"
@@ -18,7 +20,11 @@ RESET = "\033[0m"
 ANSI_PASS = f"{GREEN}\u2713{RESET}"
 ANSI_FAIL = f"{RED}\u2717{RESET}"
 
+success = []
+fail = []
+
 def parse(path):
+    global success, fail
     r = parseDir(path)
     # if an infofile was found, check if it passes checks
     if r.get('infofile') and r.get('info'):
@@ -27,10 +33,13 @@ def parse(path):
             print(r)
         if len(r['info']['tracks']) != len(r['files']):
             print(f"{ANSI_FAIL} Track length doesn't match number of files ({len(r['info']['tracks'])}:{len(r['files'])})")
+            fail.append(r['cwd'])
         elif not r['info']['metadata']['venue'] or not r['info']['metadata']['location'] or not r['info']['metadata']['date']:
-            print(f"{ANSI_FAIL} Metadata coult not be parsed")
+            print(f"{ANSI_FAIL} Metadata could not be parsed")
+            fail.append(r['cwd'])
         else:
             print(f"{ANSI_PASS} {r['info']['metadata']['artist']}: {r['info']['metadata']['date'].strftime('%Y-%m-%d')}: {r['info']['metadata']['venue']}, {r['info']['metadata']['location']}")
+            success.append(r['cwd'])
             if WRITE_TAGS:
                 for t in r['info']['tracks']:
                     writeMetaFlac(t)
@@ -48,7 +57,7 @@ def parseDir(path):
     # check for a .txt file
     for f in filelist:
         f_lower = f.lower()
-        if f.endswith(".txt") and not ".ffp" in f:
+        if f.endswith(".txt") and f != 'shntool.txt' and not ".ffp" in f and not "ffp-" in f:
             infofile = os.path.join(path, f)
         if f.endswith(EXT):
             files.append(os.path.join(path, f))
@@ -80,7 +89,18 @@ class InfoState(Enum):
     INTRO = 1
     TRACK = 2
 
+
 def parseInfoFile(filename):
+    """ Parse the infofile for album metadata and track names """
+    def _format_loc(loc):
+        """ Helper function to format localities """
+        city = loc.group('city').strip()
+        state = loc.group('state').strip()
+        if len(state) == 2:
+            state = state.upper()
+        if city[0].islower():
+            city = city[0].upper() + city[1:]
+        return f"{city}, {state}"
     linecount = 0
     tracknum = 1
     state = InfoState.INTRO
@@ -103,16 +123,20 @@ def parseInfoFile(filename):
         except:
             return {}
         for line in lines.split("\n"):
-            # linecount += 1
             line = line.strip()
             if not line:
                 continue
             linecount += 1
             if state == InfoState.INTRO:
-                # if DEBUG:
-                #     print(f"{linecount}: {line}")
                 if linecount == 1:
-                    metadata['artist'] = line
+                    END_DELIMIT = ['(', '[']
+                    for e in END_DELIMIT:
+                        line = line.split(e, 1)[0].strip()
+                    if utils.NORMALIZE.get(line.lower()):
+                        artist = utils.NORMALIZE[line.lower()]
+                    else:
+                        artist = line.title()
+                    metadata['artist'] = artist
                     continue
                 if not metadata['date']:
                     try:
@@ -124,15 +148,37 @@ def parseInfoFile(filename):
                     l1, l2 = line.split(",",1)
                     l1 = l1.strip()
                     l2 = l2.strip()
-                    if LOCATION_RE.match(l1):
-                        metadata['location'] = l1
+                    loc = LOCATION_RE.match(l1)
+                    if loc:
+                        metadata['location'] = _format_loc(loc)
                         continue
-                    if LOCATION_RE.match(l2):
+                    loc = LOCATION_RE.match(l2)
+                    if loc:
+                        metadata['venue'] = l1
+                        metadata['location'] = _format_loc(loc)
+                        continue
+                    if l2.lower() in utils.STATES or l2.lower() in utils.COUNTRIES:
+                        metadata['location'] = line
+                        continue
+                    if "," in l2 and (
+                            l2.split(",",1)[1].strip().lower() in utils.STATES or
+                            l2.split(",",1)[1].strip().lower() in utils.COUNTRIES):
                         metadata['venue'] = l1
                         metadata['location'] = l2
                         continue
+                # formats like "Deer Creek-Noblesville, IN"
+                if "-" in line and not metadata['location']:
+                    l1, l2 = line.split("-",1)
+                    l1 = l1.strip()
+                    l2 = l2.strip()
+                    loc = LOCATION_RE.match(l2)
+                    if loc:
+                        metadata['venue'] = l1
+                        metadata['location'] = _format_loc(loc)
+                        continue
                 if not metadata['location'] and LOCATION_RE.match(line):
-                    metadata['location'] = line
+                    loc = LOCATION_RE.match(line)
+                    metadata['location'] = _format_loc(loc)
                     continue
                 if not metadata['venue']:
                     metadata['venue'] = line
@@ -143,20 +189,32 @@ def parseInfoFile(filename):
                 # Skip disc intros, eg "1 Disc" or "CD 1"
                 # print(line)
                 m = DISC_RE.search(line)
-                if m:
+                if m and not 'intro' in line.lower() and not 'disco' in line.lower():
                     continue
+                # if the line is a date, skip it
+                try:
+                    _date = dateparse(line)
+                    continue
+                except Exception as e:
+                    pass
                 # Extract track information via regex
                 m = TITLE_RE.match(line)
                 # print(m)
                 if m:
+                    name = m.group('name').strip(" -<>[]\t")
+                    if name.islower():
+                        name = name.title()
                     track = {
                       'num': int(m.group('num')),
-                      'name': m.group('name').strip(" -<>[]\t"),
+                      'name': name,
                       'time': m.group('time'),
                       'track': tracknum,
                     }
                     tracknum += 1
                     tracks.append(track)
+    # clean up venue and location formatting
+    if metadata.get('venue') and metadata['venue'].islower():
+        metadata['venue'] = metadata['venue'].title()
     metadata['tracktotal'] = len(tracks)
     return {
         'tracks': tracks,
@@ -200,7 +258,7 @@ def writeMetaFlac(track):
     f.close()
 
 def main():
-    global DEBUG, WRITE_TAGS
+    global DEBUG, WRITE_TAGS, success, fail
     parser = argparse.ArgumentParser(description='Parse track info for live recordings.')
     parser.add_argument('path', nargs='?', default=os.getcwd())
     parser.add_argument('-w', '--write', action='store_true', help='write tags to flac files.')
@@ -209,6 +267,10 @@ def main():
     DEBUG = args.debug
     WRITE_TAGS = args.write
     r = parse(args.path)
-
+    print(f"{len(success)}/{len(success)+len(fail)} succeeded")
+    if len(fail) > 0:
+        print("Failed:")
+        fail_str = '\n'.join(fail)
+        print(f"{fail_str}")
 if __name__== "__main__":
     main()
